1a15af5d3bdb71d3ccdb8d8d7d6c4b0717b159b8
Tarun Kumar <tarun.mewara06@gmail.com>
2023-10-31 18:09:40 +0530

    Implement Airspeed Gauge

---
+++ Guages.ControLib/Converter/DialTextPositionConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Guages.ControLib.Converter
+{
+    public class DialTextPositionConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            double dblVal = (double)value;
+            TransformGroup tg = new TransformGroup();
+            RotateTransform rt = new RotateTransform();
+            TranslateTransform tt = new TranslateTransform();
+
+            tt.Y = dblVal;
+            tg.Children.Add(rt);
+            tg.Children.Add(tt);
+
+            return tg;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
---
+++ Guages.WPF/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Guages.WPF
+{
+    internal class MainWindowViewModel : ObservableRecipient
+    {
+        public MainWindowViewModel()
+        {
+            Score = 235;
+        }
+
+        public int Score { get; set; }
+    }
+}
